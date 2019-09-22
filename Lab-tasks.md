a70870baf494b5f8902c855e65c0979a2fd97644
Frank Ramirez <frank@edynamics.co>
2019-09-22 16:10:38 -0400

    initial commit

---
+++ Lab-tasks.md
@@ -0,0 +1,60 @@
+# Task 1
+
+Connect your Particle Argon board to the ADXL362 accesory board
+
+# Task 2
+
+After cloning project https://github.com/iot-newhaven/lab1-tempsensor.git compile local application and flash local to the board.
+
+# Task 3
+
+Add the following functions to `lab1-tempSensor.ino` file
+
+```c
+float readSensorTempC(void)
+{
+    unsigned char rawTempData[2] = {0, 0};
+    short         signedTemp = 0;
+    float         tempCelsius = 0;
+
+    rawTempData[0] = (char)Temperature;
+
+    rawTempData[1] = (char)(Temperature >> 8);
+    
+    signedTemp = (short)(rawTempData[1] << 8) + rawTempData[0];
+    
+    tempCelsius = (float)signedTemp * 0.065;
+
+    return tempCelsius;
+}
+
+void displaySensorTemp(float tempCelsius)
+{
+    static char messageBuffer[255];
+
+    snprintf(messageBuffer, sizeof(messageBuffer), "Temperature %f C\n", tempCelsius);
+
+    Serial.print(messageBuffer);
+
+    delay(1000);
+}
+```
+
+Modify your `loop()` function as shown below
+
+```c
+void loop()
+{
+    // The core of your code will likely live here.
+    
+    // Read Sensor Data 
+    getRawSensorData();
+
+    // Display to Serial Monitor
+    //displayRawSensorData();
+
+    displaySensorTemp(readSensorTempC());
+    
+}
+```
+
---
+++ src/lab1-tempSensor.cpp
@@ -0,0 +1,109 @@
+/******************************************************/
+//       THIS IS A GENERATED FILE - DO NOT EDIT       //
+/******************************************************/
+
+#include "application.h"
+#line 1 "/Users/frank/OneDrive/UNH/ELEC-6671_4501_IOT/particle/lab1-tempSensor/src/lab1-tempSensor.ino"
+/*
+ * Project lab1-tempSensor
+ * Description:
+ * Author:
+ * Date:
+ */
+
+void setup();
+void loop();
+void getRawSensorData(void);
+void displayRawSensorData(void);
+float readSensorTempC(void);
+void displaySensorTemp(float tempCelsius);
+#line 8 "/Users/frank/OneDrive/UNH/ELEC-6671_4501_IOT/particle/lab1-tempSensor/src/lab1-tempSensor.ino"
+SYSTEM_MODE(SEMI_AUTOMATIC);
+
+#include <adxl362.h>
+
+ADXL362 sensor;
+
+int16_t temp;
+
+int16_t XValue, YValue, ZValue, Temperature;
+
+// setup() runs once, when the device is first turned on.
+void setup()
+{
+    // Put initialization like pinMode and begin functions here.
+
+    Serial.begin(115200);
+
+    sensor.begin(SS); // Setup SPI protocol, issue device soft reset
+
+    sensor.beginMeasure(); // Switch ADXL362 to measure mode
+
+    Serial.println("Lab1: temp sensor");
+}
+
+// loop() runs over and over again, as quickly as it can execute.
+void loop()
+{
+    // The core of your code will likely live here.
+    
+    // Read Sensor Data 
+    getRawSensorData();
+
+    //displayRawSensorData();
+
+    displaySensorTemp(readSensorTempC());
+    
+}
+
+void getRawSensorData(void)
+{
+    // read all three axis in burst to ensure all measurements correspond to same sample time
+    sensor.readXYZTData(XValue, YValue, ZValue, Temperature);
+}
+
+void displayRawSensorData(void)
+{
+    Serial.print("XVALUE=");
+    Serial.print(XValue);
+    
+    Serial.print("\tYVALUE=");
+    Serial.print(YValue);
+    
+    Serial.print("\tZVALUE=");
+    Serial.print(ZValue);
+    
+    Serial.print("\tTEMPERATURE=");
+    Serial.println(Temperature);
+
+    delay(100);
+}
+
+
+float readSensorTempC(void)
+{
+    unsigned char rawTempData[2] = {0, 0};
+    short         signedTemp = 0;
+    float         tempCelsius = 0;
+
+    rawTempData[0] = (char)Temperature;
+
+    rawTempData[1] = (char)(Temperature >> 8);
+    
+    signedTemp = (short)(rawTempData[1] << 8) + rawTempData[0];
+    
+    tempCelsius = (float)signedTemp * 0.065;
+
+    return tempCelsius;
+}
+
+void displaySensorTemp(float tempCelsius)
+{
+    static char messageBuffer[255];
+
+    snprintf(messageBuffer, sizeof(messageBuffer), "Temperature %f C\n", tempCelsius);
+
+    Serial.print(messageBuffer);
+
+    delay(1000);
+}
